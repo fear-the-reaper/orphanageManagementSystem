93e5645154c17ff4ce5041f20254257c19f8531f
hussainKhan1999 <mshk9991@gmail.com>
2021-12-23 20:26:20 +0500

    UI things

---
+++ src/main/java/businessLogic/ChildrenCatalogue.java
@@ -0,0 +1,17 @@
+package businessLogic;
+
+import java.util.List;
+
+public class ChildrenCatalogue {
+	List<Child> childrenlist;
+
+	public void getAllchildren() {
+		
+		//store all available children from db in childrenlist
+		// ready for adoption
+		
+	}
+	
+	
+
+}
